894762b4168027106e09614465e9e79d7e6e697d
habiba97q <habiba97q@gmail.com>
2026-09-11 12:38:38 +0600

    nav bar complated

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,11 +4,11 @@ import logoText from "../assets/logo-text.png";
 const Nav = () => {
   return (
 
-    <nav className=" bg-red-100 py-6">
+    <nav className="  py-6">
       <div className="container mx-auto flex justify-between">
         <img src={logoText} alt="" />
 
-        <ul className="flex gap-4 items-center font-medium text-[#475569]">
+        <ul className="flex gap-8 items-center font-medium text-[#475569]">
           <li className="text-[#D91B7E]">Home</li>
           <li>Technologies</li>
           <li>Projects</li>
@@ -17,7 +17,7 @@ const Nav = () => {
         </ul>
 
         <div>
-          <button className="btn btn-active btn-error bg-red-100 text-[#334155] font-medium rounded-2xl border-none">Sign In</button>
+          <button className="btn btn-active btn-error bg-white text-[#334155] font-medium rounded-2xl border-none">Sign In</button>
           <button className="btn btn-active btn-error px-7 rounded-full bg-[#D91B7E] text-white">Sign Up</button>
         </div>
 
